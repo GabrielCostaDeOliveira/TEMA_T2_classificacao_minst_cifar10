90a2061e122d3778fd4ee6772bdc4c1729f9f8a5
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2024-09-12 17:37:46 -0300

    fix data

--- src/dataloader/data.py
+++ src/dataloader/data.py
@@ -1,7 +1,22 @@
 import pandas as pd
 from sklearn.datasets import load_digits
 
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.datasets import cifar10
+
 def minst():
     digits = load_digits()
 
-    return digits.images.reshape((len(digits.images), -1)), digits.target
+    x, y = digits.images.reshape((len(digits.images), -1)), digits.target
+
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y);
+
+    return x_train, x_test, y_train, y_test
+
+
+
+def cifar10():
+    x_train, y_train, x_test, y_test = cifar10.load_data()
+
+    return x_train, x_test, y_train, y_test
