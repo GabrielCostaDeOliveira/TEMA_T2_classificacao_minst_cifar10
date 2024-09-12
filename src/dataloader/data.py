953a671ac41a0e9b9e63b9b9529f495d5c6e162e
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2024-09-12 19:19:36 -0300

    fix cifar10 loader

--- src/dataloader/data.py
+++ src/dataloader/data.py
@@ -3,6 +3,8 @@ from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.datasets import cifar10
 
+from sklearn.decomposition import PCA
+
 def minst():
     digits = load_digits()
 
@@ -17,16 +19,17 @@ def minst():
 def cifar():
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-    x_train = x_train[:x_train.shape[0] // 2]
-    y_train = y_train[:y_train.shape[0] // 2]
-    x_test = x_test[:x_test.shape[0] // 2]
-    y_test = y_test[:y_test.shape[0] // 2]
 
     x_train = x_train.reshape(x_train.shape[0], -1) 
     x_test = x_test.reshape(x_test.shape[0], -1)
 
     y_train = y_train.ravel() 
     y_test = y_test.ravel()  
+
+
+    pca = PCA(n_components=0.95)  # Manter 95% da variância
+    x_train = pca.fit_transform(x_train_flattened)
+    x_test = pca.transform(x_test_flattened)
 
     return x_train, x_test, y_train, y_test
 
