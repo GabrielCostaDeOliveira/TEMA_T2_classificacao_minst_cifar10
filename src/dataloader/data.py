b297a43f38a97a7acb7f9e3718f1a7df69998c28
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2024-09-12 15:46:50 -0300

    fix minst

--- src/dataloader/data.py
+++ src/dataloader/data.py
@@ -1,11 +1,7 @@
 import pandas as pd
 from sklearn.datasets import load_digits
 
-def minst() -> pd.DataFrame:
+def minst():
     digits = load_digits()
 
-    # Correcting the syntax and formatting
-    return pd.DataFrame({
-        'x': list(digits.images.reshape((len(digits.images), -1))/255),  # Added the missing parenthesis and comma
-        'y': digits.target
-    })
+    return digits.images.reshape((len(digits.images), -1)), digits.target
--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
-import os
-
 from models.models import Models
 from dataloader.data import minst
+
+import os
 
 from sklearn.model_selection import train_test_split
 
 
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
 
 
-df = minst();
+x, y = minst();
 
-x_train, x_test, y_train, y_test = train_test_split(df.x, df. y);
+x_train, x_test, y_train, y_test = train_test_split(x, y);
 
 ## carregando modelos
 print("carregando modelos")
@@ -26,9 +26,21 @@ for name, clf in models.get_grid_search():
 
     clf.fit(x_train, y_train);
 
-    predictions = clf.predict(x_test)
 
+    predictions = clf.predict(x_test)
     report = classification_report(y_test, predictions)
+    conf_matrix = confusion_matrix(y_test, predictions)
+    best_params = clf.best_params_
+    
+
+    print("report")
+    print(report)
+
+    print("best params")
+    print(clf.best_params_)
+
+    print("conf_matrix")
+    print(conf_matrix)
 
 
     ## save classification_report
@@ -36,13 +48,10 @@ for name, clf in models.get_grid_search():
     if not os.path.exists(path):
         os.makedirs(path)
 
-    print(report)
 
     with open (path + "_classification_report.txt", 'w') as f:
         f.write(report)
 
-    ## save params
-    print("Best parameters: \n", clf.best_params_)
 
     with open(path + "_params" + '.txt', 'w') as f:
         f.write(str(clf.best_params_))
